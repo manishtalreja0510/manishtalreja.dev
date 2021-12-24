bf676a496163cadda97e1983080bef8f21306881
Manish <manishtalreja0510@gmail.com>
2021-12-24 17:35:51 +0530

    initial commit

--- android/app/src/main/java/dev/manishtalreja/website_manish_talreja/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package dev.manishtalreja.website_manish_talreja;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/constants/data.dart
+++
@@ -1,7 +0,0 @@
-Map<String,dynamic> data = {
-  'fname': 'Manish',
-  'lname': 'Talreja',
-  'designation': 'Software Developer',
-  'shortDescription': 'I am an Mobile and Web Developer. I\'m the founder of Karwaan007. I ❤️ to code and Design creative things.',
-};
-
